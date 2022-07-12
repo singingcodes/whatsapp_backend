182a7815e6b2cceba231af5b76bee5bf28f2e941
Sarah Ayanwale <sarahayanwale@gmail.com>
2022-07-12 16:22:06 +0200

    added socket.io

--- src/socket/index.js
+++ src/socket/index.js
@@ -28,15 +28,15 @@ const connectionHandler = (socket) => {
     socket.broadcast.emit("newConnection", onlineUsers) // We want to emit this event to every connected socket but not the current one
   })
 
-  socket.on("sendMessage", async ({ message, room }) => {
+  socket.on("sendMessage", async ({ message }) => {
     // we should broadcast that message to everybody but not to the sender of the message (otherwise he would see a duplicated message on the chat)
     // socket.broadcast.emit("message", message)
 
     // we would like to save the message in db
-    await saveMessage(message, room)
+    await saveMessage(message)
 
     // we would like to emit to everybody who is in the room
-    socket.to(room).emit("message", message)
+    socket.emit("message", message)
   })
 
   socket.on("disconnect", () => {
